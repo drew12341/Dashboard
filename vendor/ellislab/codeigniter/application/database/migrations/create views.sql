7e8a163fefc78256a2e4eb251bbd60687e932bf9
Andrew <andrew.alger@sinapse.com.au>
2021-04-12 15:15:14 +1200

    Update view - calculating incorrectly

--- vendor/ellislab/codeigniter/application/database/migrations/create views.sql
+++ vendor/ellislab/codeigniter/application/database/migrations/create views.sql
@@ -1,6 +1,8 @@
 create view indicator_measures_aggregate as
   select period, indicatorid, description, type,
-         case type when 'Percentage' then avg(indicator_measures.value) else total(indicator_measures.value) end as value,
+         case type when 'Percentage' then avg(indicator_measures.value)
+                   when 'Calculated' then avg(indicator_measures.value)
+                   else total(indicator_measures.value) end as value,
         total(indicator_measures.staff_in_group) as staff_in_group,
          total(indicator_measures.completions) as completions
   from indicator_measures inner join indicators on indicator_measures.indicatorid = indicators.id where category = 'standard'
