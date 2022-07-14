63827c47612c0b752f371a00d4fc7000f99eebe7
Andrew Alger <alger.andrew@gmail.com>
2022-07-15 11:14:30 +1200

    Updates for Dashboard2022

--- vendor/ellislab/codeigniter/application/config/DASH_config.php
+++ vendor/ellislab/codeigniter/application/config/DASH_config.php
@@ -8,10 +8,11 @@ $config['indicator_threshold'] = 10;
 
 $config['dash_periods']     = 'Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec';
 
-$config['1_minimise_injuries_maximise_wellbeing'] = 'Minimise Injuries / Maximise Wellbeing';
-$config['2_safe_workplace']             = 'Safe Workplace ';
-$config['3_informed_and_engaged']       = 'Informed and Engaged Workforce';
-$config['4_proactive_approach']         = 'Proactive Approach to Health, Safety and Wellbeing';
+$config['1_management_commitment_and_leadership']   = 'Management Commitment and Leadership';
+$config['2_consultation_and_communication']         = 'Consultation and Communication';
+$config['3_information_and_training']               = 'Information and Training';
+$config['4_risk_management']                        = 'Risk Management';
+$config['5_monitoring_reporting_and_verification']  = 'Monitoring, Reporting and Verification';
 
 $config['9_local_indicator']            = 'Local H&S Indicators';
 
